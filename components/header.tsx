11d4d1175f345a1705872e8d35d5d104d4971fd2
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2023-10-25 10:11:20 +0200

    fix: type

--- components/header.tsx
+++ components/header.tsx
@@ -22,28 +22,27 @@ export async function Header() {
   return (
     <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
       <div className="flex items-center">
-        
-          <Sidebar>
-            <React.Suspense fallback={<div className="flex-1 overflow-auto" />}>
-              {/* @ts-ignore */}
-              <SidebarList userId="1" />
-            </React.Suspense>
-            <SidebarFooter>
-              <ThemeToggle />
-              <ClearHistory clearChats={clearChats} />
-            </SidebarFooter>
-          </Sidebar>
-        
+        <Sidebar>
+          <React.Suspense fallback={<div className="flex-1 overflow-auto" />}>
+            {/* @ts-ignore */}
+            <SidebarList userId="1" />
+          </React.Suspense>
+          <SidebarFooter>
+            <ThemeToggle />
+            <ClearHistory clearChats={clearChats} />
+          </SidebarFooter>
+        </Sidebar>
+
         <div className="flex items-center">
           <IconSeparator className="w-6 h-6 text-muted-foreground/50" />
-         
-            <UserMenu user={{
+
+          <UserMenu
+            user={{
               image: null,
-              id: '1',
               name: 'Test',
               email: 'test@test.com'
-            }} />
-         
+            }}
+          />
         </div>
       </div>
       <div className="flex items-center justify-end space-x-2">
